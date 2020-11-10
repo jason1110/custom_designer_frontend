fde5f365096db9355e13beabf43032b1db008ccb
jason1110 <Jason.easterly11@gmail.com>
2020-11-10 11:02:53 -0700

    add image assets

--- index.js
+++ index.js
@@ -24,31 +24,36 @@ function showProductCard(eachProduct){
     const productCard = document.createElement('div')
     const cardTitle = document.createElement('h4')
     const cardImage = document.createElement('img')
+    const createButton = document.createElement('button')
     productCard.classList.add('card')
+    createButton.classList.add('btn')
+    createButton.classList.add('btn-primary')
+    createButton.classList.add('create-button')
     cardTitle.className = ('card-title')
     cardImage.className =('card-img-top')
     cardTitle.textContent = eachProduct.name
+    createButton.innerText = 'Create'
 
     switch(eachProduct.product_type){
         case "Mens-Shirt":
-            cardImage.src='./assets/images/tshirt_men/blue_mens_tshirt.png'
+            cardImage.src='./assets/images/tshirt_men/mens_tshirt.png'
             break
         case "Womens-Shirt":
-            cardImage.src='./assets/images/tshirt_womens/green_womens_vneck_tshirt.png'
+            cardImage.src='./assets/images/tshirt_womens/womens_vneck_tshirt.png'
             break
         case "LS-Shirt":
-            cardImage.src='./assets/images/longsleeve/orange-long-sleeve.png'
+            cardImage.src='./assets/images/longsleeve/white-longsleeve.png'
             break
         case "Hat":
-            cardImage.src='./assets/images/hat/red-hat.png'
+            cardImage.src='./assets/images/hat/white-hat.png'
             break
         case "Coffee Mug":
-            cardImage.src='./assets/images/mug/grey_mug.png'
+            cardImage.src='./assets/images/mug/white-mug.png'
             break;
         case "mask":
-            cardImage.src='./assets/images/mask/yellow_mask.png'
+            cardImage.src='./assets/images/mask/white-mask.png'
             break;
     }
-    productCard.append(cardImage, cardTitle)
+    productCard.append(cardImage, cardTitle, createButton)
     cardContainer.appendChild(productCard)
 }
