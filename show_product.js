a974109345efdec2d134e652317b2e8e2fd99e4f
jason1110 <Jason.easterly11@gmail.com>
2020-11-10 16:38:07 -0700

    create show product and create page

--- show_product.js
+++ show_product.js
@@ -1,26 +1,23 @@
-const baseURL = "http://localhost:4000"
-const productURL = `${baseURL}/products`
-
+const searchParams = new URLSearchParams(window.location.search);
+const product_id = searchParams.get('product_id');
+const productURL = `http://localhost:4000/products/${product_id}`
 
 const cardContainer = document.querySelector('.card-container')
-
-
-
+//color wheel
+new ReinventedColorWheel({
+    appendTo: document.getElementById('color-wheel')
+})
 
 fetch(productURL)
 .then(parseJSON)
-.then(getProducts)
+.then(getProduct)
 
 function parseJSON(response){
     return response.json()
 }
 
-function getProducts(allProducts){
-    console.log(allProducts)
-    allProducts.products.forEach(showProductCard)
-}
-
-function showProductCard(eachProduct){
+function getProduct(product){
+    console.log(product.product_type)
     const productCard = document.createElement('div')
     const cardTitle = document.createElement('h4')
     const cardImage = document.createElement('img')
@@ -31,10 +28,9 @@ function showProductCard(eachProduct){
     createButton.classList.add('create-button')
     cardTitle.className = ('card-title')
     cardImage.className =('card-img-top')
-    cardTitle.textContent = eachProduct.name
-    createButton.innerText = 'Create'
-    listenForCreate(createButton, eachProduct)
-    switch(eachProduct.product_type){
+    cardTitle.textContent = product.name
+    
+    switch(product.product_type){
         case "Mens-Shirt":
             cardImage.src='./assets/images/tshirt_men/mens_tshirt.png'
             break
@@ -53,13 +49,9 @@ function showProductCard(eachProduct){
         case "mask":
             cardImage.src='./assets/images/mask/white-mask.png'
             break;
-    }
-    productCard.append(cardImage, cardTitle, createButton)
+
+        }
+    productCard.append(cardImage, cardTitle)
     cardContainer.appendChild(productCard)
 }
 
-function listenForCreate(createButton, eachProduct){
-    createButton.addEventListener('click', (e) => {
-        document.location.href = `showProduct.html?product_id=${eachProduct.id}`
-    })
-}
